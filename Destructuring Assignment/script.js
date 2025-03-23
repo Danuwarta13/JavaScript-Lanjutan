9508ed4bc5e3dba94af216180e6e2e7a550dfd86
Danuwarta13 <danujr650@gmail.com>
2025-03-23 23:45:16 +0700

    Destructuring Object

--- Destructuring Assignment/script.js
+++ Destructuring Assignment/script.js
@@ -1,6 +1,7 @@
 // Destructuring  Variabel / Assignment
 
-// Destructuring Array
+/////////////// Destructuring Array ///////////////
+
 const perkenalan = ["Halo", "nama", "saya", "Rhinozz"];
 
 // Teknik Biasa menggunakan Index dari Array
@@ -52,5 +53,105 @@ const [coba1, coba2] = coba();
 const [angk, ...values] = [1, 2, 3, 4, 5];
 // console.log(angk); // 1
 // console.log(values); // [2, 3, 4, 5]
+
+////////////////////////////////////////////////
+
+/////////////// Destructuring Object ///////////////
+
+const mhs = {
+  id: 13,
+  myname: "Rhinozz",
+  age: 20,
+  email: "Rhinoz969@gmail.com",
+};
+
+// Teknik Biasa
+const saya = mhs.myname;
+const hidup = mhs.age;
+// console.log(saya); // Rhinozz
+// console.log(hidup); // 20
+
+////////////////////////////////////////////////
+
+// Menggunakan Destructuring
+
+// const mhs = {
+//   id: 13,
+//   myname: "Rhinozz",
+//   age: 20,
+//   email: "Rhinoz969@gmail.com",
+// };
+
+const { myname, age } = mhs;
+// console.log(myname); // Rhinozzz
+// console.log(age); // 20
+
+////////////////////////////////////////////////
+
+// Assignment tanpa deklarasi Object
+
+({ hewan, dia } = { hewan: "kucing", dia: "Rhinozz" });
+// console.log(hewan); // kucing
+// console.log(dia); // Rhinozzz
+
+////////////////////////////////////////////////
+
+// Assign ke variable baru
+
+// const mhs = {
+//   id: 13,
+//   myname: "Rhinozz",
+//   age: 20,
+//   email: "Rhinoz969@gmail.com",
+// };
+
+const { myname: n, age: u } = mhs;
+// console.log(n); // Rhinozzz
+// console.log(u); // 20
+
+////////////////////////////////////////////////
+
+// Memberikan Default Value + Assign ke variabel baru
+
+// const mhs = {
+//   id: 13,
+//   myname: "Rhinozz",
+//   age: 20,
+//   email: "Rhinoz969@gmail.com",
+// };
+
+const { myname: nick, age: ages, email: e = "emailDefault@ac.id" } = mhs;
+// console.log(e); // Rhinoz969@gmail.com
+
+////////////////////////////////////////////////
+
+// Rest parameter
+
+// const mhs = {
+//   id: 13,
+//   myname: "Rhinozz",
+//   age: 20,
+//   email: "Rhinoz969@gmail.com",
+// };
+
+const { myname: namaku, ...isi } = mhs;
+// console.log(isi); // {id:13, age : 20, email: }
+
+////////////////////////////////////////////////
+
+// Mengambil field pada object, setelah dikirim sebagai parameter untuk function
+
+// const mhs = {
+//   id: 13,
+//   myname: "Rhinozz",
+//   age: 20,
+//   email: "Rhinoz969@gmail.com",
+// };
+
+function getIdMhs({ id }) {
+  return id;
+}
+
+console.log(getIdMhs(mhs)); // 13
 
 ////////////////////////////////////////////////
